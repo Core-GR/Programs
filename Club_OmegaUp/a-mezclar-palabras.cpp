599c0356d398e6f4191621b2ede733efa2348393
Core-GR <54444246+Core-GR@users.noreply.github.com>
2019-11-03 21:46:24 -0600

    nm

---
+++ Club_OmegaUp/a-mezclar-palabras.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+//prototipo
+int main()
+{
+	string cadena1;
+	string cadena2;
+	string cadena3;
+	cin>>cadena1>>cadena2>>cadena3;
+	string cadenaf;
+	if (cadena1.size()<=1000001 && cadena2.size()<=1000001 && cadena3.size()<=1000001)
+	{
+		cadenaf=cadena1+cadena2+cadena3;
+		sort(cadenaf.begin(),cadenaf.end());
+		int m=cadenaf.size();
+		m=(m/2)+0.5;
+		cout<<cadenaf[m]<<endl;
+	}
+	return 0;
+}
